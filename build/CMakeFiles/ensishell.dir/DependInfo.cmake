738946db2dfc983b64974bbd2bbbfca634f52a0b
Brazouskaya Darya <Darya.Brazouskaya@ensimag.imag.fr>
2015-03-01 16:29:13 +0100

    ololo

---
+++ build/CMakeFiles/ensishell.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/user/4/.base/brazousd/home/TPshell/src/ensishell.c" "/user/4/.base/brazousd/home/TPshell/build/CMakeFiles/ensishell.dir/src/ensishell.c.o"
+  "/user/4/.base/brazousd/home/TPshell/src/readcmd.c" "/user/4/.base/brazousd/home/TPshell/build/CMakeFiles/ensishell.dir/src/readcmd.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /user/4/.base/brazousd/home/TPshell
+# Build directory: /user/4/.base/brazousd/home/TPshell/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(UnitShellTests "../tests/allShellTests.rb")
